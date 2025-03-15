9691ddffa628376e2f8c68439bcd86a44eaf3f38
Marko Heijnen <marko.heijnen@jamf.com>
2025-03-15 16:46:23 +0100

    Add logo

--- client/src/views/404View.vue
+++ client/src/views/404View.vue
@@ -3,6 +3,6 @@
   headline="Whoops, 404"
   title="Page not found"
   text="The page you were looking for does not exist"
-  image="https://vuetifyjs.b-cdn.net/docs/images/logos/v.png"
+  image="/logo.png"
 ></v-empty-state>
 </template>
